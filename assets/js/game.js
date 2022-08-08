59c59defba0bc9107144fb0963d00cb3fa80eb48
NinaSerebryakova <serebr.nina@gmail.com>
2022-08-08 17:29:56 -0400

    log examples

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");  
+}
+//fight();
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("Our robot's name is " + playerName);
+
